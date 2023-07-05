6f26caecad3e814491e663db9fc397b0c83929d8
AllenJoeG <allen.joeg@gmail.com>
2023-07-05 15:39:27 -0500

    React Query implemented in Details component.

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # Reactv8FEM
+
 Currently holding notes taken during class. To be cleaned up at a later date.
 
-## shell commands
+## various shell commands run during tutorial
 
 pass on debug to eslint, without the '--' it will pass '--debug' to npm
 
 - npm run lint -- --debug
 
-Vite
+### Vite
 
 - npm i -D vite@3.1.4 @vitejs/plugin-react@2.1.0
 
-React Addons
+### React + more
 
 - npm i react@18.2.0 react-dom@18.2.0
 - npm i react-router-dom@6.4.1
 - npm i @tanstack/react-query@4.10.1
 
-ESLint
+### ESLint
 
 - npm i -D eslint-plugin-import@2.26.0 eslint-plugin-jsx-a11y@6.6.1 eslint-plugin-react@7.31.8
-- npm i -D eslint-plugin-react-hooks@4.6.0  
+- npm i -D eslint-plugin-react-hooks@4.6.0
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,42 @@
 import { useParams } from 'react-router-dom';
+import {useQuery} from '@tanstack/react-query';
+import fetchPet from './fetchPet';
 
 const Details = () => {
   const { id } = useParams();
-  return <h2> {id} </h2>
+  
+  //React Query allows us to use asynchronous things as if they were synchronous state calls. Browse all the awesome results.options
+  //pass arg1 key string, arg2 id as spec in func
+  const results = useQuery(["details", id], fetchPet);
+  //cute hacky Loading using react query result status. Code won't proceed until result complete
+  if (results.isLoading) {
+    return(
+      <div className="loading-pane">
+        <h2 className="loader"> 💫 </h2>
+      </div>
+    )
+  }
+
+  //error handling template from React Query
+  if (results.isError) {
+    return (
+      <div> Error </div>
+    )
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1> {pet.name} </h1>
+        <h2>
+          {pet.animal} -- {pet.breed} -- {pet.city}, {pet.state} 
+          <button> Adopt {pet.name} </button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
+    </div>
+  )
 }
 export default Details;
